bd916214fd76b6335096203668bd18c61be6e931
LEN-TRUBO\neo <fantian_0033@163.com>
2016-10-28 11:10:28 +0800

    update

--- neo-bat/ren.bat
+++ neo-bat/ren.bat
@@ -1,21 +1,32 @@
 @echo off 
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::批量修改当前脚本所在目录下的文件名称，将名称前四位2014替换成2016
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
 setlocal enableDelayedExpansion
 for %%m in (*) do (
+		set /a all+=1
+		
     if not "%%m"=="%~nx0" (
     	if not "%%~xm" == "bat" (
-    		set /a num+=1
-    		rem set sstr=%%~nm
-    		rem set sstr=!sstr:~3！
-        rem ren "%%m" "!num!!sstr!%%~xm"
-        set str=%%m
-        set str=!str:~4!
-        echo "2016!str!%%~xm"
-        rem &set str=!str:~1! 
-   			rem ren "%%a" "!str!" 
+    		set sstr=%%~nm
+    		set sstr=!sstr:~0,4!
+        rem echo #Test: !all! !sstr! %%~xm
+        
+        if "!sstr!"=="2014" (
+        	set /a num+=1
+        	set str=%%m
+        	set str=!str:~4!
+        	rem echo "2016!str! %%~xm"
+        	ren "%%m" "2016!str!"
+        ) 
     	)
     )
+    
 )
-echo 改名完成！
+echo "共 !all! 文件  !num! 个文件改名完成"
 pause
 
 
